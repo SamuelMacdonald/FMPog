5581b949d1c530ecc0da59e95de5e1824f8a89bf
SamuelMacdonald <146819235+SamuelMacdonald@users.noreply.github.com>
2024-05-20 15:44:51 +0100

    Clown update

---
+++ Assets/Scripts/enemyAi.cs
@@ -0,0 +1,127 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float health;
+
+    public Animator anim;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //states
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            Patroling();
+        }
+
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+    }
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+        {
+            agent.SetDestination(walkPoint);
+        }
+       // anim.SetBool("Run", true);
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+      //  anim.SetBool("Run", false);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 1f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        anim.SetBool("Attack", false);
+        //  anim.SetBool("Run", true);
+    }
+
+    private void AttackPlayer()
+    {
+
+        //Attack code goes here
+       anim.SetBool("Attack", true);
+
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+       
+    }
+   
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), 2);
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, sightRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/gun.cs
@@ -0,0 +1,38 @@
+
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public float damage = 10;
+    public float range = 100f;
+    public float fireRate = 100f;
+
+    public Camera fpsCam;
+    private float nextTimeToFire = 0f;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f/fireRate;
+            Shoot();
+        }
+    }
+
+
+    public void Shoot()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            enemyHealth enemy = hit.transform.GetComponent<enemyHealth>();
+
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+    }
+}
